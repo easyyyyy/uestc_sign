ff759ad286d7523296172424cd98a03e233fdc03
xixixi <526282887@qq.com>
2021-06-16 11:40:05 +0800

    logs

--- cookie.js
+++ cookie.js
@@ -1,4 +1,8 @@
 module.exports = {
+<<<<<<< Updated upstream
   cookie: '', // 填写fiddler获取的cookie
   PUSH_PLUS_TOKEN: '' // 填写pushplus的token(选填)
+=======
+  cookie: 'JSESSIONID=60ee3117-9499-4f8e-8ab9-f14b632d8833'
+>>>>>>> Stashed changes
 }
