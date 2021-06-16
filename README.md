cbaffd2cb247847d9a1f904d914624579486f321
huangyixi <huangyixi@fenbi.com>
2021-06-16 11:42:38 +0800

    11

---
+++ README.md
@@ -0,0 +1,9 @@
+## 食用方法
+- 微信pc版打开uestc学生情况小程序，fiddler抓包获取cookie填入cookie.js
+- 安装node
+- 安装forever后台挂起node程序
+  ```
+  npm i forever -g
+  ```
+- 
+- [fiddler抓包](https://juejin.cn/post/6844904042422861831)
--- cookie.js
+++ cookie.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< Updated upstream
   cookie: '', // 填写fiddler获取的cookie
   PUSH_PLUS_TOKEN: '' // 填写pushplus的token(选填)
-=======
-  cookie: 'JSESSIONID=60ee3117-9499-4f8e-8ab9-f14b632d8833'
->>>>>>> Stashed changes
 }
