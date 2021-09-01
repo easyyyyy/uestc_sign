1106da43f786697629204b2a2a7423ce4343b197
easy <60744475+easyyyyy@users.noreply.github.com>
2021-09-01 13:14:36 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,2 +1,2 @@
-15 0 */1 * * /usr/local/bin/node /uestc_sign/sign.js >> /uestc_sign/logs/sign.log 2>&1
+15 10 */1 * * /usr/local/bin/node /uestc_sign/sign.js >> /uestc_sign/logs/sign.log 2>&1
 #* * * * * /usr/local/bin/node /uestc_sign/test.js >> /uestc_sign/logs/test.log 2>&1
