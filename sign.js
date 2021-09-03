fe449c69adab4de4a19e913863d2e4acc1d45727
easy <60744475+easyyyyy@users.noreply.github.com>
2021-09-03 17:58:31 +0800

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,5 +1,6 @@
 const request = require('./axios.js');
 const { pushPlusNotify } = require('./sendNotify.js');
+const { cookie } = require('./cookie.js');
 
 (async function() {
   const url = 'http://jzsz.uestc.edu.cn/wxvacation/monitorRegisterForReturned';
@@ -13,16 +14,24 @@ const { pushPlusNotify } = require('./sendNotify.js');
     }
   }
   console.log(params)
-  const res = await request(url, 'POST', params)
-  console.log(res)
-  if(res.code == 0) {
-    console.log('==========签到成功============')
-    await pushPlusNotify('签到成功', '123')
-  } else if (res.code == 40001) {
-    console.log('===============登录失效，请重新登录================')
-    await pushPlusNotify('cookie失效', 'cookie过期，请重新获取cookie填入cooke.js')
-  } else {
-    console.log('=============其他错误================')
-    await pushPlusNotify('其他错误', JSON.stringify(res))
-  }
+  const cookies = cookie.split(',')
+  cookies.forEach((item) => {
+    const options = {
+      headers: {
+        'Cookie': item,
+      }
+    }
+    const res = await request(url, 'POST', params, options)
+    console.log(res)
+    if(res.code == 0) {
+      console.log('==========签到成功============')
+      await pushPlusNotify('签到成功', '123')
+    } else if (res.code == 40001) {
+      console.log('===============登录失效，请重新登录================')
+      await pushPlusNotify('cookie失效', 'cookie过期，请重新获取cookie填入cooke.js')
+    } else {
+      console.log('=============其他错误================')
+      await pushPlusNotify('其他错误', JSON.stringify(res))
+    }
+  })
 })()
